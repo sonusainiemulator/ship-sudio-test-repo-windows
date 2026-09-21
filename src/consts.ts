1867bf9c4da3ba88c7b4130ce781518fbfe45e89
TTinfotechs01 <161292390+TTinfotechs01@users.noreply.github.com>
2026-09-21 16:26:01 +0530

    Add dedicated Contact Us page with embedded Google Map for Bhadra office and 5.0-star rating verification

--- src/consts.ts
+++ src/consts.ts
@@ -30,3 +30,31 @@ export const SOCIAL_LINKS = {
   github: "https://github.com/rakebigservices",
 };
 
+// Official Physical Office Addresses & Google Map Locations
+export const HEAD_OFFICE = {
+  name: "Rakebig advertisement agency / Rakebig Services",
+  street: "Ward No.25, near Bsnl Office, Saini Mohalla",
+  locality: "Bhadra",
+  region: "Rajasthan",
+  postalCode: "335501",
+  country: "India",
+  countryCode: "IN",
+  fullAddress: "Ward No.25, near Bsnl Office, Saini Mohalla, Bhadra, Rajasthan 335501",
+  googleMapsEmbed: "https://maps.google.com/maps?q=Ward+No.25,+near+Bsnl+Office,+Saini+Mohalla,+Bhadra,+Rajasthan+335501&t=&z=16&ie=UTF8&iwloc=&output=embed",
+  googleMapsUrl: "https://www.google.com/search?sca_esv=ef3eb8aeaef7656a&sxsrf=APpeQnv4NkghYc4pBqPP31wX3wA2hisCvQ:1789987619544&q=rakebig+advertisement+agency+bhadra+address&ludocid=5144840765114331929&sa=X&ved=2ahUKEwj7gNzBv_-WAxWD1jgGHZP0L2wQ6BN6BAgxEAI",
+  rating: 5.0,
+  reviewsCount: 10,
+  hours: "Open 24 hours",
+};
+
+export const UK_OFFICE = {
+  name: "Rakebig LTD",
+  street: "7 Barnard Road",
+  locality: "Enfield",
+  region: "Greater London",
+  postalCode: "EN1 3QA",
+  country: "United Kingdom",
+  countryCode: "GB",
+  fullAddress: "7 Barnard Road, Enfield EN1 3QA, Greater London, UK",
+};
+
